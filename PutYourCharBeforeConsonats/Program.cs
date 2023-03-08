ae0f0c96f840f1e5c6728592fb33564489114d4b
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-08 11:07:23 -0300

    Outra forma de resolver o algoritmo em conjunto com a turma

--- PutYourCharBeforeConsonats/Program.cs
+++ PutYourCharBeforeConsonats/Program.cs
@@ -1,28 +1,41 @@
-﻿string originalPhrase;
-string newPhrase = "";
-string consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
-char userCharactere;
+﻿string originalPhrase = "", newPhrase = "";
+//string consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+//char userCharactere;
 
-UserInteraction();
-CreateString(originalPhrase, userCharactere);
+//UserInteraction();
+originalPhrase = (Console.ReadLine());
+CreateString(originalPhrase);
 Console.WriteLine("Translating...\n\n\n");
 Console.Write("Your phrase: {0}\n\n\n\n\n", newPhrase);
 
-string CreateString(string original, char charac)
+//void UserInteraction()
 {
-    foreach (char c in original)
+    // Console.WriteLine("1st put your phrase here: ");
+    // originalPhrase = Console.ReadLine();
+    // Console.WriteLine("Now, put the character you want to add");
+    //userCharactere = (char)(Console.Read());
+}
+string CreateString(string original)
+{
+    char[] aux = new char[original.Length * 2];
+    for (int i = 0, j = 0; i < original.Length; i++)
     {
-        if (consonants.Contains(c))
-            newPhrase += c + charac.ToString();
+        if ((original[i] != 'a') && (original[i] != 'e') && (original[i] != 'i') && (original[i] != 'o') && (original[i] != 'u') &&
+            (original[i] != 'A') && (original[i] != 'E') && (original[i] != 'I') && (original[i] != 'O') && (original[i] != 'U'))
+        {
+            aux[j + 1] = original[i];
+            aux[j] = '.';
+            j += 2;
+        }
         else
-            newPhrase += c.ToString();        
+        {
+            aux[j] = original[i];
+            j++;
+        }
+    }
+    for (int i = 0; i < aux.Length; i++)
+    {
+        newPhrase += aux[i];
     }
     return newPhrase;
-}   
-void UserInteraction()
-{
-    Console.WriteLine("1st put your phrase here: ");
-    originalPhrase = Console.ReadLine();
-    Console.WriteLine("Now, put the character you want to add");
-    userCharactere = (char)(Console.Read());
 }
